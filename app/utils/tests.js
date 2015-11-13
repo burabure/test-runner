a8f65d97a225310f202f20865d43debfb4611320
BuraBure <nicolas@santiagolab.cl>
2015-11-13 15:02:00 -0300

    change delay back to original

--- app/utils/tests.js
+++ app/utils/tests.js
@@ -1,5 +1,5 @@
 function generateDummyTest() {
-  var delay = 100 + Math.random() * 100; //7000
+  var delay = 7000 + Math.random() * 7000;
   var testPassed = Math.random() > 0.5;
 
   return function(callback) {
