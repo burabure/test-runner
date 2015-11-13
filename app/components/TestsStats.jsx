846e3d9a9cea4f516d8d765e9e8a2f8e0c58f4c7
BuraBure <nicolas@santiagolab.cl>
2015-11-13 15:58:54 -0300

    nice test runner bar!

--- app/components/TestsStats.jsx
+++ app/components/TestsStats.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 const TestsStats = ({stats, totalTests}) => {
   const hasFinished = (stats.passed + stats.failed === totalTests);
-  
+
+  const displayRunnerBar = `[${String(
+                            ' . '.repeat(totalTests) +
+                            ' · '.repeat(stats.passed + stats.failed))
+                          .slice(-(totalTests * 3))
+                          .split('')
+                          .reverse()
+                          .join('')
+                          }]`;
+
   return (
     <div>
       <h3>Stats:</h3>
+      <h4>{displayRunnerBar} {hasFinished ? 'Finished!' : null}</h4>
       Running: {stats.running} / {totalTests}
       {' | '}
       Passed: {stats.passed} / {totalTests}
       {' | '}
       Failed: {stats.failed} / {totalTests}
-
-      {hasFinished ? ' | Finished!' : null}
     </div>
   );
 };
